73a347c04b7c8b94718bb87335bdb4d613d7c334
Aman Bhatnagar <amanbhatnagar351@gmail.com>
2025-03-13 17:30:24 +0530

    import react added to solve error

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,6 +1,7 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import { JSX } from "react";
+import React from "react";
 
 const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
   const { user } = useAuth();
